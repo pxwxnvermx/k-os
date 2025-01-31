954728043f74a712a1cfa535b968991ee13e43db
pxwxnvermx <pawanjbs5@gmail.com>
2025-01-31 18:24:51 +0530

    Add kernel end

--- src/main.rs
+++ src/main.rs
@@ -5,36 +5,23 @@ mod multiboot;
 mod mutex;
 mod vga_buffer;
 
-use core::fmt::Write;
 use core::{arch::global_asm, panic::PanicInfo};
-use multiboot::{MultibootInfo, MultibootMMapEntry};
+use multiboot::MultibootInfo;
 
 global_asm!(include_str!("boot.S"), options(att_syntax));
+
+extern "C" {
+    static end_of_kernel: u32;
+}
 
 #[no_mangle]
 pub extern "C" fn kernel_main(multiboot_info: *const MultibootInfo, magic: u32) -> ! {
     vga_buffer::WRITER.lock().clean_buffer();
+    unsafe { (*multiboot_info).print_info() };
+    println!("Magic bytes: {}", magic);
     unsafe {
-        for i in 0..(*multiboot_info).mmap_addr {
-            let mmmt = ((*multiboot_info).mmap_addr
-                + core::mem::size_of::<MultibootMMapEntry>() as u32 * i)
-                as *const MultibootMMapEntry;
-            let len = (*mmmt).len_low;
-            let size = (*mmmt).size;
-            if size == 0 {
-                break;
-            }
-            writeln!(
-                vga_buffer::WRITER.lock(),
-                "size: {}, len: {}, addr: {}, type: {}",
-                size,
-                len,
-                (*mmmt).addr_low,
-                (*mmmt).mem_type
-            )
-            .unwrap();
-        }
-    }
+        print!("Kernel End: {:p}", &end_of_kernel);
+    };
     loop {}
 }
 
